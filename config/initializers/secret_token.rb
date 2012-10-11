f4d76fd9a714ad42f6b8bfd2902c75b3c10e81b3
Ben Shyong <likeachild@gmail.com>
2012-10-10 20:42:19 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Labstyle::Application.config.secret_token = '47afc541e5355fb39023ecce44ed235ab3515e104d768f0a25970f8e340812ca4bcfabd7153937f9432a249dd6691a8e8fa7b81a080c3d6575559880001fdc11'
